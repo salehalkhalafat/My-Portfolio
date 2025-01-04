cffd0b7a25a8795e98a11e271808505336900612
salehalkhalafat <salehalkhalafat144@gmail.com>
2025-01-04 17:36:44 +0300

    fixed statics

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -116,12 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles/',
+    BASE_DIR / 'static/',
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
