ba4b3b621829512e40776f01c7bd3c8e03262772
salehalkhalafat <salehalkhalafat144@gmail.com>
2025-01-04 20:34:40 +0300

    fixed statics

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -119,8 +119,6 @@ USE_TZ = True
 STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
