b21a4f1f0cb32e48dc2df039da24830e61fdc16b
salehalkhalafat <salehalkhalafat144@gmail.com>
2025-01-04 18:22:05 +0300

    fixed statics

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -117,9 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/staticfiles/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static/',
-]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
